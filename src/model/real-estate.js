405887f49eab60017573b388822f08583465574d
Séra Bálint <sera.balint@e-vista.hu>
2017-05-27 18:03:53 +0200

    uniq id

--- src/model/real-estate.js
+++ src/model/real-estate.js
@@ -1,5 +1,8 @@
+const uniqueString = require('unique-string')
+
 class RealEstate {
   constructor (address, price, shortDesc, size, roomCount, foreignID) {
+    this._id = uniqueString()
     this.address = address
     this.price = price
     this.shortDesc = shortDesc
