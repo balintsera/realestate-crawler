750134735843450c21ca89ef26b61699d0c704b2
Séra Bálint <sera.balint@e-vista.hu>
2017-06-03 15:22:41 +0200

    REST refresh

--- index.js
+++ index.js
@@ -1,23 +1,43 @@
 const DOMCrawler = require('./src/service/dom-crawler')
 const urls = require('./src/config/urls.js')
 const elasticsearch = require('elasticsearch')
+
+
 const esClient = new elasticsearch.Client({
-  host: '127.0.0.1:9200',
-  log: 'error'
+ host: '127.0.0.1:9200',
+ log: 'error'
 })
+
 const BulkInsert = require('./src/service/elastic-bulk-insert')
 
-esClient.indices.delete({ index: 'flats' })
+const restify = require('restify')
 
-const crawler = new DOMCrawler(
+function refresh (req, res, next) {
+  esClient.indices.delete({ index: 'flats' })
+
+  const crawler = new DOMCrawler(
     urls,
     (results) => {
-        if (results.length > 0) {
-            new BulkInsert('flats', 'flat', results, esClient)
-        }
+      if (results.length > 0) {
+        new BulkInsert('flats', 'flat', results, esClient)
+      }
     })
 
-crawler
+  crawler
     .start()
+  next()
+}
+
+const server = restify.createServer()
+server.pre(restify.pre.userAgentConnection())
+server.get('/refresh', refresh)
+server.head('/refresh', refresh)
+
+server.listen(8080, function () {
+  console.log('%s listening at %s', server.name, server.url)
+})
+
+
+
 
 
